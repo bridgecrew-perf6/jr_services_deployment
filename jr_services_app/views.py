c2340113b16e9501524114a63ab01bddd3b06f72
jerenteria <renteria.juan96@gmail.com>
2022-01-10 10:03:34 -0800

    initial commit

---
+++ jr_services_app/views.py
@@ -0,0 +1,147 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+import bcrypt
+from .models import *
+from flask import Flask, url_for, render_template, request, abort
+from .static import *
+import stripe
+
+app = Flask(__name__)
+
+app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51K2mNoIuO4V9XiaIZxiAWZ8T5yL4bDfygzyKoScqC6zqI6bgsi31XPYOqlCIE2HR0Gx2bCfndIMdm9W0B44r8Cw1009w5WnBex'
+app.config['STRIPE_SECRET_KEY'] = 'sk_test_51K2mNoIuO4V9XiaIb1QuwuZFfL66qeCCpOfmp8F0WzHV72gXpKTrcthFIVAXcXb374ox6OvMlZ2esP0YCJtHlj7c00MjtJnz1w'
+
+stripe.api_key = app.config['STRIPE_SECRET_KEY']
+@app.route('/')
+def stripe():
+    '''
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': 'price_1K32ZWIuO4V9XiaI1EfUtljP',
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('index', _external=True),
+    )
+    '''
+    return render_template(
+        'make_payment.html', 
+        #checkout_session_id=session['id'], 
+        #checkout_public_key=app.config['STRIPE_PUBLIC_KEY']
+    )
+
+@app.route('/stripe_pay')
+def stripe_pay():
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price': 'price_1K32ZWIuO4V9XiaI1EfUtljP',
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url=url_for('thanks', _external=True) + '?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url=url_for('make_payment', _external=True),
+    )
+    return {
+        'checkout_session_id': session['id'], 
+        'checkout_public_key': app.config['STRIPE_PUBLIC_KEY']
+    }
+
+
+
+@app.route('/thanks')
+def thanks():
+    return render_template('thanks.html')
+
+def index(request):
+    return render(request, "index.html")
+
+def success(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    return render(request, "home.html")
+
+@app.route('/stripe_webhook', methods=['POST'])
+def stripe_webhook():
+    print('WEBHOOK CALLED')
+
+    if request.content_length > 1024 * 1024:
+        print('REQUEST TOO BIG')
+        abort(400)
+    payload = request.get_data()
+    sig_header = request.environ.get('HTTP_STRIPE_SIGNATURE')
+    endpoint_secret = 'whsec_dqKTOFBLyEfLv1KGQKot7ideIkF1vqB9'
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        print('INVALID PAYLOAD')
+        return {}, 400
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        print('INVALID SIGNATURE')
+        return {}, 400
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+        print(session)
+        line_items = stripe.checkout.Session.list_line_items(session['id'], limit=1)
+        print(line_items['data'][0]['description'])
+
+    return {}
+
+def register(request):
+    if request.method=='POST':
+        # validate the data
+        errors=User.objects.validator(request.POST)
+        if errors:
+            for error in errors:
+                messages.error(request, errors[error])
+            return redirect("/")
+        ## encrypting our password
+        ## store plaintext password in variable
+        user_pw=request.POST['pw']
+        ## hash the password
+        hash_pw=bcrypt.hashpw(user_pw.encode(), bcrypt.gensalt()).decode()
+        #test
+        print(hash_pw)
+        new_user=User.objects.create(first_name=request.POST['f_n'], last_name=request.POST['l_n'], email=request.POST['email'], password=hash_pw)
+        print(new_user)
+        request.session['user_id']=new_user.id
+        request.session['user_name']=f"{new_user.first_name} {new_user.last_name}"
+        return redirect('/home')
+    return redirect('/')
+
+def aboutus(request):
+    return render(request, "aboutus.html")
+
+
+def login(request):
+    ## loging a user in
+    if request.method=='POST':
+        ## query for logged user
+        logged_user=User.objects.filter(email=request.POST['email'])
+        if logged_user:
+            logged_user=logged_user[0]
+            ## compare the passwords
+            if bcrypt.checkpw(request.POST['pw'].encode(), logged_user.password.encode()):
+                request.session['user_id']=logged_user.id
+                request.session['user_name']=f"{logged_user.first_name} {logged_user.last_name}"
+                return redirect('/home')
+    return redirect('/')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def makePayment(request):
+    return render(request,"make_payment.html")
+
+
